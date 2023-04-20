542933220e0b1ca22fa9a6f10284953ea6f10d4b
KU10S <shamilvkage@gmail.com>
2023-04-20 14:03:48 +0530

    new change

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const { Server } = require('socket.io');
 
-const io = new Server(8000, 
-    {
+const io = new Server(8000, {
         cors: true,
     });
 
+    const emailtosocketidMap = new Map();
+    const socketidtoEmailMap = new Map();
+
 io.on("connection", (socket) => {
     console.log(`Socket Connected`, socket.id);
-})
+    socket.on('room:join',(data) => {
+        const { email,room } = data;
+        emailtosocketidMap.set(email, socket.id);
+        socketidtoEmailMap.set(socket.id, email);
+        io.to(socket.id).emit('room:join', data);
+    });
+});
