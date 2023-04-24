e8da060eb42b25ffa684f40364f4f4303283729b
KU10S <shamilvkage@gmail.com>
2023-04-24 12:30:23 +0530

    implementing socket

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ io.on("connection", (socket) => {
         const { email,room } = data;
         emailtosocketidMap.set(email, socket.id);
         socketidtoEmailMap.set(socket.id, email);
+        io.to(room).emit("user:joined", { email, id: socket.id });
+        socket.join(room);
         io.to(socket.id).emit('room:join', data);
     });
 });
